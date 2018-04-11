06ad50fafb40353754c8156588f28a9338663f37
xpc <xinpeicheng@yeah.net>
2018-04-11 17:01:08 +0800

    增加问卷题目组件

---
+++ src/components/writeQuestionnaire/INPUT.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" :class="$style.ipt">
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang='stylus' module>
+.ipt
+ height 24px
+ box-sizing border-box
+ margin 10px 0px
+ border 1px solid #999
+ padding-left 4px
+ border-radius 4px
+</style>
--- src/components/writeQuestionnaire/Questionnaire.vue
+++ src/components/writeQuestionnaire/Questionnaire.vue
@@ -3,6 +3,24 @@
     <ul :class="$style.fulllist">
       <li v-for="(item,index) in wjList" :key="item.id" :class="$style.list">
         <p :class="$style.listtitle">{{index+1}}．{{item.wtName}}</p>
+        <div :class="$style.problem" v-for="wjItem in brands" :key="wjItem.id">
+
+          <div :class="$style.brandname">
+            {{wjItem.name}}
+          </div>
+          <div v-if="item.wtType=='TEXT'" :class="$style.brandwt">
+            <INPUT v-for="wtitem in item.items" :key="wtitem.id"></INPUT>
+          </div>
+          <div v-if="item.wtType=='STAR'" :class="$style.brandwt">
+            {{item.wtType}}
+          </div>
+          <div v-if="item.wtType=='RADIO'" :class="$style.brandwt">
+            {{item.wtType}}
+          </div>
+          <div v-if="item.wtType=='TEXTAREA'" :class="$style.brandwt">
+            <TEXTAREA></TEXTAREA>
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -10,6 +28,10 @@
 
 <script>
 import WjData from '@/assets/data/Questionnaire.json'
+import { mapState } from 'vuex'
+import INPUT from '@/components/writeQuestionnaire/INPUT'
+import TEXTAREA from '@/components/writeQuestionnaire/TEXTAREA'
+import RADIO from '@/components/writeQuestionnaire/RADIO'
 export default {
   name:"questionnaire",
   data() {
@@ -18,9 +40,18 @@ export default {
     }
   },
   created(){
-    console.log(this.list)
+    console.log(this.wjList)
   },
-  methods:{}
+  methods:{},
+  computed:{
+    ...mapState({
+      brands:state =>state.brands
+    })
+  },
+  components:{
+    INPUT,
+    TEXTAREA
+  }
 }
 </script>
 
@@ -38,4 +69,15 @@ export default {
   padding 10px
   box-sizing border-box
   border-bottom 1px solid #eeeeee
+ .problem
+  border-bottom 1px solid #eeeeee
+  box-sizing border-box
+  padding 0 10px
+  display flex
+  justify-content space-between
+  align-items center
+  .brandname
+   color red
+  .brandwt
+   color blue
 </style>
