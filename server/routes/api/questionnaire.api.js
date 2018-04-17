36c33746fe16a721ab2a9f2a7652a0f98f885c6b
xpc <xinpeicheng@yeah.net>
2018-04-17 10:03:17 +0800

    增加接口：获取品牌列表　获取问题列表

--- server/routes/api/questionnaire.api.js
+++ server/routes/api/questionnaire.api.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 class Questionnaire{
   static async getbrand(ctx){
-    ctx.body = '666'
+    //ctx.body = '666'
+  },
+  static async getproblem(ctx){
+    //返回操作
   }
 }
 
--- server/routes/route.js
+++ server/routes/route.js
@@ -3,6 +3,6 @@ const questionnaire = require('./api/questionnaire.api')
 
 router
   .get('/api/getbrand', questionnaire.getbrand)
-
+  .post('/api/getproblem', questionnaire.getproblem)
 
 module.exports = router;
